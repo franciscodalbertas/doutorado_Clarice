8cd75e1f21e81a6a701b43406719f4876f7f2a14
Francisco d'Albertas <francisco.albertas@gmail.com>
2021-10-25 18:38:42 -0300

    ressucitando

--- trading_LR.R
+++ trading_LR.R
@@ -40,93 +40,102 @@ names(df)[c(24:26)] <- c("riqueza_aves_cl","conect_PC400m_cl","polinizacao_cl")
 df2 <-df %>% arrange(desc(area_exced_foraRL))
 
 
+# a funcao esta descontando infinitamente os deficits, mas precisa parar em 0!
+
 #==== encontrando hexagonos equivalentes =======================================
 
 # aqui estou considerando apenas compensacao
 
 
 i=317 #bom pra testar
+i=1574 # id 1446
+# lista_exedente <- list() # lista vazia para guardar os resultados
+# lista_deficit <- list()
 
-lista_exedente <- list() # lista vazia para guardar os resultados
-lista_deficit <- list()
+# c <- 0
+# c_2 <- 0
 
-c <- 0
-c_2 <- 0
 
-# a funcao tem q ser iterativa e ja modificar o df original!
-# isso implica que nao da pra guardar o resultado numa lista!
+
+df3 <- df2 # df para ser modificado pela funcao!
 
 for(i in seq(1,nrow(df2))){
   
-  c <- c +1 # contador pra lista de hexagonos com excedente
   
-  hex <- df2[i,]
   
-  #---- subset hexagonos  com mesmos valores de classe -------------------------
   
-  # selecionando apenas hexagonos com deficit
-  eq <- df2[df2$riqueza_aves_cl==hex$riqueza_aves_cl&
-             df2$conect_PC400m_cl==hex$conect_PC400m_cl&
-             df2$polinizacao_cl==hex$polinizacao_cl&df2$area_deficit_oficial>0,]
-  
-  if(length(eq)==0){
-    hex$valor_comp <- 0
-    hex$area_exced_foraRL_nv <- hex$area_exced_foraRL
-    hex$id_hex_def <- NA
-    lista_exedente[[c]] <- hex
-  } else{
-  #---- ordenando hexagonos equivalentes pela area de deficit ------------------
-    eq <- eq %>% arrange(desc(area_deficit_oficial))
+    hex <- df3[i,]
+    # adicionar algo q se o excedente for ==0, pular
+    if(hex$area_exced_foraRL==0){next}else{
     
-  #---- selecionando valores pra serem compensados -----------------------------
+      #---- subset hexagonos  com mesmos valores de classe -------------------------
     
-    # se a area de deficit for maior ou igual  a area de excedente:
-    if (eq$area_deficit_oficial>=hex$area_exced_foraRL){
+    # selecionando apenas hexagonos com deficit
+    eq <- df3[df3$riqueza_aves_cl==hex$riqueza_aves_cl&
+               df3$conect_PC400m_cl==hex$conect_PC400m_cl&
+               df3$polinizacao_cl==hex$polinizacao_cl&df3$area_deficit_oficial>0,]
+    
+    if(nrow(eq)==0){
+      # substituindo df original!
+      df3$valor_comp[df3$id_hexagono==hex$id_hexagono] <- 0
+      df3$id_hex_def[df3$id_hexagono==hex$id_hexagono] <- NA
       
-      c_2 <- c_2+1 # contador pra lista de hexagonos com deficit compensados
+    } else{
+    #---- ordenando hexagonos equivalentes pela area de deficit ------------------
+      eq <- eq %>% arrange(desc(area_deficit_oficial))
       
-      # selecionar apenas 1 hexagono
+    #---- selecionando valores pra serem compensados -----------------------------
       
-      comp <- eq[1,]
- 
-      #---- atualizando valores do dataframe -----------------------------------
-      
-      # criando novo objeto com o hexagono com excedente
-      hex2 <- hex
-      # descontando valor compensado
-      hex2$area_exced_foraRL_nv <- 0
-      # valor compensado
-      hex2$valor_comp <- hex$area_exced_foraRL*hex$preco_compensacao
-      hex2$id_hex_def <- comp$id_hexagono
-      # descontando valor deficit!
-      comp$area_deficit_oficial_nv <- comp$area_deficit_oficial-hex$area_exced_foraRL
-      lista_exedente[[c]] <- hex2
-      lista_deficit <- comp[[c_2]]
-      
-    }else{
-        # se a area de deficit for menor q a de excedente
-        #continuar
-      
+      # se a area de deficit do primeiro hex for maior ou igual  a area de excedente:
+      if (eq$area_deficit_oficial[1]>=hex$area_exced_foraRL){
+        
+        # selecionar apenas 1 hexagono
+        
+        def <- eq[1,] # deficit a ser compensado ( ta errado pq nao pode ter deficit 0)
+   
+        #---- atualizando valores do dataframe -----------------------------------
+        # descontando valor compensado
+        df3$area_exced_foraRL[df3$id_hexagono==hex$id_hexagono] <- 0
+        # valor compensado
+        df3$valor_comp[df3$id_hexagono==hex$id_hexagono] <- hex$area_exced_foraRL*hex$preco_compensacao
+        df3$id_hex_def[df3$id_hexagono==hex$id_hexagono] <- def$id_hexagono
+        # descontando valor deficit! aqui precisa ser no hexagono q foi compensado!
+        df3$area_deficit_oficial[df3$id_hexagono==def$id_hexagono] <- 
+        df3$area_deficit_oficial[df3$id_hexagono==def$id_hexagono]-
+        hex$area_exced_foraRL
+        # lista_exedente[[c]] <- hex2
+        # lista_deficit <- comp[[c_2]]
+        
+      }else{
+          
+        print("vai dar certo")
+        
+        }
       }
+    }
     
-  }
-  
-  #---- ordenando pelo valor do deficit ----------------------------------------
-  
-  
-  
-  
-  
-  # #---- armazenando ids dos hexagonos ----------------------------------------
-  # 
-  # ids <- eq$id_hexagono # quais ids sao equivalentes
-  # hex$eq_hex_ids <- paste(ids,collapse = ",") # inserindo ids em uma coluna
-  # hex$n_eq <- length(ids) # colocando n de hexagonos equivalentes
-  # #---- guarando resultados na lista vazia
-  lista_hexagonos[[i]] <- hex
+    #---- ordenando pelo valor do deficit ----------------------------------------
+    
+    
+    
+    
+    
+    # #---- armazenando ids dos hexagonos ----------------------------------------
+    # 
+    # ids <- eq$id_hexagono # quais ids sao equivalentes
+    # hex$eq_hex_ids <- paste(ids,collapse = ",") # inserindo ids em uma coluna
+    # hex$n_eq <- length(ids) # colocando n de hexagonos equivalentes
+    # #---- guarando resultados na lista vazia
+    lista_hexagonos[[i]] <- hex
 }
 
-df3 <- do.call(rbind,lista_hexagonos)
-                     
-hist(df3$area_comp)            
-summary(as.factor(df3$area_comp))         
+
+# checando output
+
+h_1446_in <- df2[df2$id_hexagono=="1446",]
+
+h_1446 <- df3[df3$id_hexagono=="1446",]
+
+# parece Ok, falta agora para os hexagonos q a area de deficit eh menor q o 
+# excedente, pra fechar!
+
