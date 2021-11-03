17b4b53d49d903559f37a0e8bcd9c8cecc8d92cd
Francisco d'Albertas <francisco.albertas@gmail.com>
2021-11-03 20:48:01 -0300

    cenarios de compensacao completos

--- trading_LR.R
+++ trading_LR.R
@@ -35,12 +35,9 @@ df <- cbind(df,lapply(df[,c(21,22,23)],FUN = f)) # aplicando funcao nas metricas
 # nomeando as novas colunas com as classes
 names(df)[c(24:26)] <- c("riqueza_aves_cl","conect_PC400m_cl","polinizacao_cl")
 
-# ordenando dados pela area de excedente, decrescente
+# ordenando dados pelo custo de compensacao, decrescente
 
-df2 <-df %>% arrange(desc(area_exced_foraRL))
-
-
-# a funcao esta descontando infinitamente os deficits, mas precisa parar em 0!
+df2 <-df %>% arrange(desc(preco_CO))
 
 #==== encontrando hexagonos equivalentes =======================================
 
@@ -49,23 +46,24 @@ df2 <-df %>% arrange(desc(area_exced_foraRL))
 
 i=317 #bom pra testar
 i=1574 # id 1446
-# lista_exedente <- list() # lista vazia para guardar os resultados
-# lista_deficit <- list()
-
-# c <- 0
-# c_2 <- 0
-
-
 
 df3 <- df2 # df para ser modificado pela funcao!
 
-for(i in seq(1,nrow(df2))){
+
+# limite superior do for
+
+lim <- nrow(df2)
+lim <- 60
+
+i=61
+
+for(i in seq(1,lim)){
   
   
   
-  
+    # hex q recebera compensacao
     hex <- df3[i,]
-    # adicionar algo q se o excedente for ==0, pular
+    # se o excedente for ==0, pular
     if(hex$area_exced_foraRL==0){next}else{
     
       #---- subset hexagonos  com mesmos valores de classe -------------------------
@@ -103,41 +101,35 @@ for(i in seq(1,nrow(df2))){
         df3$area_deficit_oficial[df3$id_hexagono==def$id_hexagono] <- 
         df3$area_deficit_oficial[df3$id_hexagono==def$id_hexagono]-
         hex$area_exced_foraRL
-        # lista_exedente[[c]] <- hex2
-        # lista_deficit <- comp[[c_2]]
+        
         
       }else{
-          
-        print("vai dar certo")
+        # se a area de deficit for menor que o excedente, pode ser q um hexagono
+        # compense mais de 1, ai precisa incluir na coluna.
         
+        # area de excedente:
+        area_exced <- hex$area_exced_foraRL
+        eq$cumsum <- cumsum(eq$area_deficit_oficial)
+        # subset apenas com areas q completam o excedente. Caso nao complete, o hexagono
+        # continua na proxima
+        def <- eq[eq$cumsum<=area_exced,]
+        # area total compensada
+        area_compensada <- sum(def$area_deficit_oficial)
+        # descontando valor compensado ( ta gerando valores negativos!)
+        df3$area_exced_foraRL[df3$id_hexagono==hex$id_hexagono] <- area_exced-
+          area_compensada
+        # valor compensado
+        df3$valor_comp[df3$id_hexagono==hex$id_hexagono] <- area_compensada*hex$preco_compensacao
+        df3$id_hex_def[df3$id_hexagono==hex$id_hexagono] <- paste(eq2$id_hexagono,collapse = ",")
+        #print("vai dar certo")
+        # descontando valor deficit! compensa o deficit todo, logo é 0
+        df3$area_deficit_oficial[df3$id_hexagono %in%def$id_hexagono] <- 0
+          
         }
       }
     }
-    
-    #---- ordenando pelo valor do deficit ----------------------------------------
-    
-    
-    
-    
-    
-    # #---- armazenando ids dos hexagonos ----------------------------------------
-    # 
-    # ids <- eq$id_hexagono # quais ids sao equivalentes
-    # hex$eq_hex_ids <- paste(ids,collapse = ",") # inserindo ids em uma coluna
-    # hex$n_eq <- length(ids) # colocando n de hexagonos equivalentes
-    # #---- guarando resultados na lista vazia
-    lista_hexagonos[[i]] <- hex
 }
 
+# salvando dados pos compensacao em nova planilha
 
-# checando output
-
-h_1446_in <- df2[df2$id_hexagono=="1446",]
-
-h_1446 <- df3[df3$id_hexagono=="1446",]
-
-# parece Ok, falta agora para os hexagonos q a area de deficit eh menor q o 
-# excedente, pra fechar!
-
-nrow(df3[is.na(df3$valor_comp),])
-
+write.csv(df3,"Tabela_variaveis_cenarios_3atributos_pos_comp.csv",row.names = F)
